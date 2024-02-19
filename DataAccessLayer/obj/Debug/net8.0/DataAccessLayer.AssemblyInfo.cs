d2b3354e2a53fafb60e2485b0f00c129b41641d7
Berin <dogaalacaaa@gmail.com>
2024-02-20 00:27:34 +0300

    create generic repository

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0c5b6b79976cb95d54b119fc6032ebd7aa38fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfecbc0750866d1e4c200901cff1ab820bac75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
