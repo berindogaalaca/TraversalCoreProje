5db17d1d22b61f700399aec45683920fd82d6fc6
Berin <dogaalacaaa@gmail.com>
2024-02-20 13:35:26 +0300

    add theme

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x=>x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz!!");
+            RuleFor(x=>x.Description).MinimumLength(50).WithMessage("En Az 50 Karakter Olmalıdır.");
+            RuleFor(x=>x.Description).MaximumLength(1500).WithMessage("En Fazla 1500 Karakter Olmalıdır.");
+            RuleFor(x=>x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz...");
+        }
+    }
+}
--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01cf9814d2dd6de9e5e6b26383169a049189bc53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fb967bab20b2ee5dbac6bbd55e4be4ab085e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
